9d93d933b8385d2b54f270603355702c246c4db4
keroloussamy <keroloussamy98@gmail.com>
2022-05-15 11:45:33 +0200

    Add SinglePostPage

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 export const PostsList = () => {
   //React components can read data from the Redux store using the useSelector hook from the React-Redux library
@@ -12,6 +13,9 @@ export const PostsList = () => {
     <article className="post-excerpt" key={post.id}>
       <h3>{post.title}</h3>
       <p className="post-content">{post.content.substring(0, 100)}</p>
+      <Link to={`/posts/${post.id}`} className="button muted-button">
+        View Post
+      </Link>
     </article>
   ))
 
---
+++ src/features/posts/SinglePostPage.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+
+export const SinglePostPage = ({ match }) => {
+  const { postId } = match.params
+
+  /* 
+    The component will re-render any time the value returned from useSelector changes to a new reference.
+    So components should always try to select the smallest possible amount of data they need from the store, which will help ensure that it only renders when it actually needs to.
+  */
+  const post = useSelector(state =>
+    state.posts.find(post => post.id === postId)
+  )
+
+  if (!post) {
+    return (
+      <section>
+        <h2>Post not found!</h2>
+      </section>
+    )
+  }
+
+  return (
+    <section>
+      <article className="post">
+        <h2>{post.title}</h2>
+        <p className="post-content">{post.content}</p>
+      </article>
+    </section>
+  )
+}
