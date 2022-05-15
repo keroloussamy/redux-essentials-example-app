283b5ec5ea6bcf8ac8aa5c5e62542d02c0c1dc3d
keroloussamy <keroloussamy98@gmail.com>
2022-05-15 11:07:17 +0200

    Add the postAdded reducer

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -8,7 +8,14 @@ const initialState = [
 const postsSlice = createSlice({
   name: 'posts',
   initialState,
-  reducers: {},
+  reducers: {
+    //Here it converts those mutations into safe immutable updates internally using the Immer library
+    postAdded(state, action) {
+      state.push(action.payload)
+    }
+  },
 })
+
+export const { postAdded } = postsSlice.actions
 
 export default postsSlice.reducer
