a4d7bbb23192a4393700d12a3975f9a9b1010a18
keroloussamy <keroloussamy98@gmail.com>
2022-05-24 18:24:51 +0200

    Add addNewPost mutation

--- src/features/posts/AddPostForm.js
+++ src/features/posts/AddPostForm.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import { useSelector } from 'react-redux'
 
-import { addNewPost } from './postsSlice'
+import { Spinner } from '../../components/Spinner'
+import { useAddNewPostMutation } from '../api/apiSlice'
 import { selectAllUsers } from '../users/usersSlice'
 
 export const AddPostForm = () => {
@@ -9,42 +10,37 @@ export const AddPostForm = () => {
   const [content, setContent] = useState('')
   const [userId, setUserId] = useState('')
 
-  //We need access to the store's dispatch function. We get this by calling the useDispatch hook from React-Redux.
-  const dispatch = useDispatch()
 
+  const [addNewPost, { isLoading }] = useAddNewPostMutation()
   const users = useSelector(selectAllUsers)
 
   const onTitleChanged = (e) => setTitle(e.target.value)
   const onContentChanged = (e) => setContent(e.target.value)
   const onAuthorChanged = e => setUserId(e.target.value)
-  const [addRequestStatus, setAddRequestStatus] = useState('idle')
 
-  const canSave =
-    [title, content, userId].every(Boolean) && addRequestStatus === 'idle'
+  const canSave = [title, content, userId].every(Boolean) && !isLoading
 
   const onSavePostClicked = async () => {
     if (canSave) {
       try {
-        setAddRequestStatus('pending')
-        await dispatch(addNewPost({ title, content, user: userId })).unwrap()
+        await addNewPost({ title, content, user: userId }).unwrap()
+
         setTitle('')
         setContent('')
         setUserId('')
       } catch (err) {
         console.error('Failed to save the post: ', err)
-      } finally {
-        setAddRequestStatus('idle')
       }
     }
   }
-
-
 
   const usersOptions = users.map(user => (
     <option key={user.id} value={user.id}>
       {user.name}
     </option>
   ))
+
+  const spinner = isLoading ? <Spinner size="30px" /> : null
 
   return (
     <section>
@@ -73,7 +69,11 @@ export const AddPostForm = () => {
           value={content}
           onChange={onContentChanged}
         />
-        <button type="button" onClick={onSavePostClicked} disabled={!canSave}>Save Post</button>
+
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <button type="button" onClick={onSavePostClicked} disabled={!canSave}>Save Post</button>
+          {spinner}
+        </div>
       </form>
     </section>
   )
