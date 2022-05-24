22e031108bc75644321c2eb53ce18349d69996d3
keroloussamy <keroloussamy98@gmail.com>
2022-05-24 18:11:26 +0200

    Add getPost query endpoint

--- src/features/posts/SinglePostPage.js
+++ src/features/posts/SinglePostPage.js
@@ -1,31 +1,28 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 
 import { PostAuthor } from './PostAuthor'
-import { selectPostById } from './postsSlice'
 import { ReactionButtons } from './ReactionButtons'
 import { TimeAgo } from './TimeAgo'
+import { Spinner } from '../../components/Spinner'
+import { useGetPostQuery } from '../api/apiSlice'
 
 export const SinglePostPage = ({ match }) => {
   const { postId } = match.params
 
   /* 
+    useSelector()
     The component will re-render any time the value returned from useSelector changes to a new reference.
     So components should always try to select the smallest possible amount of data they need from the store, which will help ensure that it only renders when it actually needs to.
   */
-  const post = useSelector((state) => selectPostById(state, postId))
 
-  if (!post) {
-    return (
-      <section>
-        <h2>Post not found!</h2>
-      </section>
-    )
-  }
+  const { data: post, isFetching, isSuccess } = useGetPostQuery(postId)
 
-  return (
-    <section>
+  let content
+  if (isFetching) {
+    content = <Spinner text="Loading..." />
+  } else if (isSuccess) {
+    content = (
       <article className="post">
         <h2>{post.title}</h2>
         <div>
@@ -38,6 +35,7 @@ export const SinglePostPage = ({ match }) => {
           Edit Post
         </Link>
       </article>
-    </section>
-  )
+    )
+  }
+  return <section>{content}</section>
 }
