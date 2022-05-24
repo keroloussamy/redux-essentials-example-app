5a9f5a6e46ae0c902d09771f084b5e9c3cb9cef5
keroloussamy <keroloussamy98@gmail.com>
2022-05-24 17:42:56 +0200

    Add initial API slice with getPosts query

--- src/app/store.js
+++ src/app/store.js
@@ -3,11 +3,16 @@ import { configureStore } from '@reduxjs/toolkit'
 import postsReducer from '../features/posts/postsSlice'
 import usersReducer from '../features/users/usersSlice'
 import notificationsReducer from '../features/notifications/notificationsSlice'
+import { apiSlice } from '../features/api/apiSlice'
 
 export default configureStore({
   reducer: {
     posts: postsReducer,
     users: usersReducer,
     notifications: notificationsReducer,
+    [apiSlice.reducerPath]: apiSlice.reducer,
   },
+  //The API slice generates a custom middleware that needs to be added to the store. This middleware must be added as well - it manages cache lifetimes and expiration.
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
 })
--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -1,21 +1,14 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import React from 'react'
 import { Link } from 'react-router-dom'
 import { PostAuthor } from './PostAuthor'
-import {
-  fetchPosts,
-  selectPostIds,
-  selectPostById,
-} from './postsSlice'
+import { useGetPostsQuery } from '../api/apiSlice'
 
 import { ReactionButtons } from './ReactionButtons'
 import { TimeAgo } from './TimeAgo'
 import { Spinner } from '../../components/Spinner'
 
 
-let PostExcerpt = ({ postId }) => {
-  const post = useSelector((state) => selectPostById(state, postId))
-
+let PostExcerpt = ({ post }) => {
   return (
     <article className="post-excerpt" key={post.id}>
       <h3>{post.title}</h3>
@@ -34,30 +27,22 @@ let PostExcerpt = ({ postId }) => {
 }
 
 export const PostsList = () => {
-  const dispatch = useDispatch()
-  //React components can read data from the Redux store using the useSelector hook from the React-Redux library
-  //The "selector functions" that you write will be called with the entire Redux state object as a parameter, and should return the specific data that this component needs from the store.
-  const orderedPostIds = useSelector(selectPostIds)
-
-  const postStatus = useSelector((state) => state.posts.status)
-  const error = useSelector((state) => state.posts.error)
-
-  useEffect(() => {
-    if (postStatus === 'idle') {
-      dispatch(fetchPosts())
-    }
-  }, [postStatus, dispatch])
+  const {
+    data: posts,
+    isLoading,
+    isSuccess,
+    isError,
+    error,
+  } = useGetPostsQuery()
 
   let content
 
-  if (postStatus === 'loading') {
+  if (isLoading) {
     content = <Spinner text="Loading..." />
-  } else if (postStatus === 'succeeded') {
-    content = orderedPostIds.map((postId) => (
-      <PostExcerpt key={postId} postId={postId} />
-    ))
-  } else if (postStatus === 'failed') {
-    content = <div>{error}</div>
+  } else if (isSuccess) {
+    content = posts.map((post) => <PostExcerpt key={post.id} post={post} />)
+  } else if (isError) {
+    content = <div>{error.toString()}</div>
   }
 
   return (
